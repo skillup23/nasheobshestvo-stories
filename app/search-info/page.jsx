f9a10f8fe12f9fe95fd3843bc209b6e4f7cc04fb
Robert Anisimov <skillup23@yandex.ru>
2023-10-04 09:20:57 +0300

    Тестирование  api

--- app/search-info/page.jsx
+++ app/search-info/page.jsx
@@ -1,10 +1,37 @@
-import SearchBar from "@/components/SearchBar";
-import React from "react";
+import SearchBar from '@/components/SearchBar';
+import React from 'react';
 
-export default function SearchInfo() {
+export const metadata = {
+  title: 'Поиск определений',
+  description: 'Поиск определений Нашего Общества',
+};
+
+// async function getData() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+//     next: { revalidate: 3600 },
+//   });
+
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data');
+//   }
+
+//   return res.json();
+// }
+
+const SearchInfo = async () => {
+  // const data = await getData();
+
   return (
-    <div className='m-8 flex flex-col justify-center max-w-lg'>
+    <div className="m-8 flex flex-col justify-center max-w-lg">
       <SearchBar />
+      {/* {data.map((item) => (
+        <div key={item.id}>
+          <h2 className="mt-3">{item.title}</h2>
+          <p>{item.body}</p>
+        </div>
+      ))} */}
     </div>
   );
-}
+};
+
+export default SearchInfo;
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,21 +1,30 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import { AiOutlineSearch } from "react-icons/ai";
-import { words } from "@/public/search-info/data";
+import useSWR from 'swr';
+import React, { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { words } from '@/public/search-info/data';
 
 function SearchBar() {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    'https://jsonplaceholder.typicode.com/posts',
+    fetcher
+  );
+
+  console.log(data);
+
   const [activeSearch, setActiveSearch] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
   const [showText, setShowText] = useState([
-    "Введите в поисковую строку название определения, которое хотите найти.",
-    "Далее выберите из выпадающео списка подходящий вариант.",
-    "Если в выпадающем списка такого нет, то данное определение отсутствует в нашем словаре.",
+    'Введите в поисковую строку название определения, которое хотите найти.',
+    'Далее выберите из выпадающео списка подходящий вариант.',
+    'Если в выпадающем списка такого нет, то данное определение отсутствует в нашем словаре.',
   ]);
 
   const handleSearch = (e) => {
     setInputValue(e.target.value);
-    if (e.target.value == "") {
+    if (e.target.value == '') {
       setActiveSearch([]);
       return false;
     }
@@ -31,31 +40,31 @@ function SearchBar() {
   const handleClick = (text) => {
     setShowText(text);
 
-    document.querySelector("input");
-    setInputValue("");
+    document.querySelector('input');
+    setInputValue('');
     setActiveSearch([]);
   };
 
   return (
-    <form className='m-w-[440px] relative'>
-      <div className='relative'>
+    <form className="m-w-[440px] relative">
+      <div className="relative">
         <input
-          type='search'
-          placeholder='Поиск определения'
-          className='w-full p-4 rounded-full bg-slate-800'
+          type="search"
+          placeholder="Поиск определения"
+          className="w-full p-4 rounded-full bg-slate-800"
           value={inputValue}
           onChange={(e) => handleSearch(e)}
         />
-        <div className='absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-slate-900 rounded-full'>
+        <div className="absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-slate-900 rounded-full">
           <AiOutlineSearch />
         </div>
       </div>
 
       {activeSearch.length > 0 && (
-        <div className='absolute top-20 p-4 bg-slate-800 text-white w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2'>
+        <div className="absolute top-20 p-4 bg-slate-800 text-white w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2">
           {activeSearch.map((s) => (
             <span
-              className='cursor-pointer'
+              className="cursor-pointer"
               key={s.title}
               onClick={() => handleClick(s.text)}
             >
@@ -64,9 +73,9 @@ function SearchBar() {
           ))}
         </div>
       )}
-      <div className='mt-8'>
+      <div className="mt-8">
         {showText.map((item, index) => (
-          <p className='mt-2' key={`${index}:${item}`}>
+          <p className="mt-2" key={`${index}:${item}`}>
             {item}
           </p>
         ))}
